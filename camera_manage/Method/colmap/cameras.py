0ddf27df8e5ef90e177effe86444161b567b3398
chLi <565353780@qq.com>
2023-10-17 22:05:04 +0800

    add convert image to RGB process as default

--- camera_manage/Method/colmap/cameras.py
+++ camera_manage/Method/colmap/cameras.py
@@ -40,6 +40,9 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder):
         image_name = os.path.basename(image_path).split(".")[0]
         image = Image.open(image_path)
 
+        if image.mode == 'L':
+            image = image.convert('RGB')
+
         cam_info = CameraInfo(uid=uid, R=R, T=T, FovY=FovY, FovX=FovX, image=image,
                               image_path=image_path, image_name=image_name, width=width, height=height)
         cam_infos.append(cam_info)
